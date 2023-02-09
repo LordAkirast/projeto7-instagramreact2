4633e8c4352f3a5419fa9b57198ba17379d528a8
LordAkirast <darkwarish@gmail.com>
2023-02-08 23:37:10 -0300

    You can now change profile pic and profile name. In stories components, suggested profiles now are components.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,55 @@
+import react from "react"
+
 export default function Sidebar() {
-    return(
-        <div class="sidebar">
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-          <div class="texto">
-            <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
-            </span>
+    const [profileName,setprofilename] = react.useState("Daniel")
+    const [profileImage,setprofileImage] = react.useState("assets/img/catanacomics.svg")
+
+    function Profile(props) {
+        return (
+            <div class="usuario">
+            <img onClick={changeImage} src={props.image} alt={props.image}/>
+            <div class="texto">
+              <span>
+                <strong>{props.name}</strong>
+                <ion-icon onClick={changeName}   name="pencil"></ion-icon>
+              </span>
+            </div>
           </div>
-        </div>
+    
+        )
+    }
+
+    function changeName() {
+        const resposta = prompt("Entre com o nome de usuário: ")
+        
+        if (resposta === "" || resposta === undefined || resposta === null) {
+            resposta = profileName
+        } else {
+            setprofilename(resposta)
+        }
+        
+        
+    }
+
+
+    function changeImage() {
+        const answer = prompt("Entre com a URL de imagem de usuário: ")
+        
+        if (answer === "" || answer === undefined || answer === null) {
+            answer = profileImage
+        } else {
+            setprofileImage(answer)
+        }
+        
+        
+    }
+
+    return(
+
+        
+        <div class="sidebar">
+            <Profile name={profileName} image={profileImage}/>
+       
 
         <div class="sugestoes">
           <div class="titulo">
@@ -17,65 +57,11 @@ export default function Sidebar() {
             <div>Ver tudo</div>
           </div>
 
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
-              <div class="texto">
-                <div class="nome">bad.vibes.memes</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
-              <div class="texto">
-                <div class="nome">chibirdart</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-              <div class="texto">
-                <div class="nome">razoesparaacreditar</div>
-                <div class="razao">Novo no Instagram</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-              <div class="texto">
-                <div class="nome">adorable_animals</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
-              <div class="texto">
-                <div class="nome">smallcutecats</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
+          <Sugestao image="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg" nome="bad.vibes.memes" razao="Segue você" follow="Seguir"/>
+          <Sugestao image="assets/img/chibirdart.svg" alt="chibirdart" nome="chibirdart" razao="Segue você" follow="Seguir"/>
+          <Sugestao image="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" nome="razoesparaacreditar" razao="Novo no Instagram" follow="Seguir"/>
+          <Sugestao image="assets/img/adorable_animals.svg" alt="adorable_animals" nome="adorable_animals" razao="Segue você" follow="Seguir"/>
+          <Sugestao image="assets/img/smallcutecats.svg" alt="smallcutecats" nome="smallcutecats" razao="Segue você" follow="Seguir"/>
         </div>
 
         <div class="links">
@@ -89,7 +75,35 @@ export default function Sidebar() {
       </div>
         
     )
+
+   
+    
+    
 }
+
+
+function Sugestao(props) {
+
+    return(
+    <div class="sugestao">
+    <div class="usuario">
+      <img src={props.image} alt={props.alt}/>
+      <div class="texto">
+        <div class="nome">{props.nome}</div>
+        <div class="razao">{props.razao}</div>
+      </div>
+    </div>
+
+    <div class="seguir">{props.follow}</div>
+  </div>
+
+)
+}
+
+
+//assets/img/catanacomics.svg
+//catanacomics
+
 
 
 
